772e249a62ee779875b1cb9514b4495936fceef8
johnson feranmi <olamilekanferanmi99@gmail.com>
2023-09-16 19:23:56 +0100

    message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 let port = 4500;
@@ -11,38 +11,116 @@ let port = 4500;
 
 let details = [
     "set",
-    {   id:1,
-        name:'femi',
-        age:20,
-        city:'ilorin'
+    {
+        id: 1,
+        name: "rice",
+        price: "1,500",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKjY9InE2215svW9oxT7WLqYan6fLX1MPQ2A&usqp=CAU"
     },
-    {   id:2,
-        name:'love',
-        age:30,
-        city:'lagos '
+    {
+        id: 2,
+        name: "rice",
+        price: "1,500",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKjY9InE2215svW9oxT7WLqYan6fLX1MPQ2A&usqp=CAU"
     },
-    {   id:3,
-        name:'femi',
-        age:20,
-        city:'ilorin'
+    {
+        id: 3,
+        name: "rice",
+        price: "1,500",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKjY9InE2215svW9oxT7WLqYan6fLX1MPQ2A&usqp=CAU"
     },
-    {   id:4,
-        name:'femi',
-        age:20,
-        city:'ilorin'
+    {
+        id: 4,
+        name: "rice",
+        price: "1,500",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKjY9InE2215svW9oxT7WLqYan6fLX1MPQ2A&usqp=CAU"
+    },
+    {
+        id: 5,
+        name: "rice",
+        price: "1,500",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKjY9InE2215svW9oxT7WLqYan6fLX1MPQ2A&usqp=CAU"
+    },
+    {
+        id: 6,
+        name: "rice",
+        price: "1,500",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKjY9InE2215svW9oxT7WLqYan6fLX1MPQ2A&usqp=CAU"
+    },
+    {
+        id: 7,
+        name: "rice",
+        price: "1,500",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKjY9InE2215svW9oxT7WLqYan6fLX1MPQ2A&usqp=CAU"
+    },
+    {
+        id: 8,
+        name: "rice",
+        price: "1,500",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKjY9InE2215svW9oxT7WLqYan6fLX1MPQ2A&usqp=CAU"
+    },
+    {
+        id: 9,
+        name: "rice",
+        price: "1,500",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKjY9InE2215svW9oxT7WLqYan6fLX1MPQ2A&usqp=CAU"
+    },
+    {
+        id: 10,
+        name: "rice",
+        price: "1,500",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKjY9InE2215svW9oxT7WLqYan6fLX1MPQ2A&usqp=CAU"
+    },
+
+
+    {
+        id: 11,
+        name: "rice",
+        price: "1,500",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKjY9InE2215svW9oxT7WLqYan6fLX1MPQ2A&usqp=CAU"
+    },
+    {
+        id: 12,
+        name: "rice",
+        price: "1,500",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKjY9InE2215svW9oxT7WLqYan6fLX1MPQ2A&usqp=CAU"
+    },
+    {
+        id: 13,
+        name: "rice",
+        price: "1,500",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKjY9InE2215svW9oxT7WLqYan6fLX1MPQ2A&usqp=CAU"
+    },
+    {
+        id: 14,
+        name: "rice",
+        price: "1,500",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKjY9InE2215svW9oxT7WLqYan6fLX1MPQ2A&usqp=CAU"
+    },
+    {
+        id: 15,
+        name: "rice",
+        price: "1,500",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKjY9InE2215svW9oxT7WLqYan6fLX1MPQ2A&usqp=CAU"
+    },
+    {
+        id: 16,
+        name: "rice",
+        price: "1,500",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKjY9InE2215svW9oxT7WLqYan6fLX1MPQ2A&usqp=CAU"
     },
 ]
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.send('hello')
 })
 
-app.get('/api', (req,res)=>{
+app.get('/api', (req, res) => {
     res.send(details)
 })
-app.post("/signup", (req,res)=>{
+app.post("/signup", (req, res) => {
     console.log(req.body);
 })
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`server is running on port ${port}`);
 })
